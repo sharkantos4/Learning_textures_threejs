efc058cb93ba385ee84c02e9c20d57df2399fb1a
Jens CT Hetland <sharkantos1@gmail.com>
2020-05-31 18:07:52 +0200

    feat: now shows rotating cube with six different textures

--- src/views/components/ShowThree.js
+++ src/views/components/ShowThree.js
@@ -13,54 +13,66 @@ class ShowThree extends React.Component {
         if (canvas) {
             var scene = new THREE.Scene();
             var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-            camera.position.set( 0, 0, 30 );
+            camera.position.set( 0, 0, 3 );
             var renderer = new THREE.WebGLRenderer({canvas});
             renderer.setSize( window.innerWidth, window.innerHeight );
 
-            var MAX_POINTS = 500;
+            const loader = new THREE.TextureLoader();
+            let cubes = [];
 
-            // geometry
-            var geometry = new THREE.BufferGeometry();
+            // Cube
+            const boxWidth = 1;
+            const boxHeight = 1;
+            const boxDepth = 1;
+            const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+            const materials = [
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-1.jpg') }),
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-2.jpg') }),
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-3.jpg') }),
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-4.jpg') }),
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-5.jpg') }),
+                new THREE.MeshBasicMaterial({ map: loader.load(process.env.PUBLIC_URL + 'flower-6.jpg') })
+            ];
+            const cube = new THREE.Mesh(geometry, materials);
+            scene.add(cube);
+            cubes.push(cube);
 
-            // attributes
-            var positions = new Float32Array( MAX_POINTS * 3 ); // 3 vertices per point
-            geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-
-            // draw range
-            var drawCount = 2; // draw the first 2 points, only
-            geometry.setDrawRange( 0, drawCount );
-
-            // material
-            var material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
-
-            // line
-            var line = new THREE.Line( geometry,  material );
-            scene.add( line );
-
-            var x, y, z, index;
-            x = y = z = index = 0;
-
-            for ( var i = 0, l = MAX_POINTS; i < l; i ++ ) {
-
-                positions[ index ++ ] = x;
-                positions[ index ++ ] = y;
-                positions[ index ++ ] = z;
-
-                x += ( Math.random() - 0.5 ) * 20;
-                y += ( Math.random() - 0.5 ) * 20;
-                z += ( Math.random() - 0.5 ) * 20;
-
-}
             
-            function animate() {
-                requestAnimationFrame( animate );
-                const newValue = Math.trunc(Math.random() * 500)
-                line.geometry.setDrawRange( 0, newValue );
+            const showthree = this;
+            function animate(time) {
+
+                time *= 0.001;
+
+                if (showthree.resizeRendererToDisplaySize(renderer)) {
+                    const canvas = renderer.domElement;
+                    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+                    camera.updateProjectionMatrix();
+                  }
                 renderer.render( scene, camera );
+                
+                cubes.forEach((cube, ndx) => {
+                    const speed = .5 + ndx * .5;
+                    const rot = time * speed;
+                    cube.rotation.x = rot;
+                    cube.rotation.y = rot;
+                  });
+
+                requestAnimationFrame( animate );
             }
-            animate();   
+            requestAnimationFrame( animate ); 
         }
     }
+
+    resizeRendererToDisplaySize = (renderer) => {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+        return needResize;
+      }
 
     render() {
         this.threejs();
